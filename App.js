6b67e978c71b3f5cdd5428d72a7e6431586adeae
Regina Wang <rrreggginnna@gmail.com>
2021-05-08 19:54:38 -0700

    cleanup for app.js

--- App.js
+++ App.js
@@ -1,40 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import Food from './Food';
 
 function App() {
+  const [foodList, setFoodList] = useState([]); // ACTIONABLE: 
+                                                // it would be cleaner if we had a seperate input bar component!
+                                                // i can explain why this would be useful/cleans up code
+  const [likes, setLikes] = useState([]);
 
-  const myFood = "Double Stuf Oreos";
+  const [inputFood, setInputFood] = useState("");
+  const [isSingleView, setIsSingleView] = useState(false);
+  const [currIndex, setCurrIndex] = useState(0);
+
+  const addFood = () => {
+    setFoodList([...foodList, inputFood]);
+    setLikes([...likes, 0]);
+    setInputFood("");
+  }
+
+  const nextFood = () => {
+    if (currIndex < foodList.length - 1) {
+      setCurrIndex(currIndex + 1);
+    } else {
+      setCurrIndex(0);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-
-        <h1> {function(myFood) {
-          return myFood + ' at midnight';
-        }(myFood)}
-        </h1>
-
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <h1>
-          Luke Dalton's First React App
-      </h1>
-
-        <p class="App">
-          Not gonna add any css to this really, I think the point was to just create a React app.
-      </p>
-
+        <div>
+          { // generally if it's only one line, i like to use an arrow function!
+            <button onClick={() => setIsSingleView(!isSingleView)}> {
+              isSingleView ? "View List" : "View Single"
+            } </button>
+          }
+          <h2> Add a New Food</h2> 
+          <input type="text" onChange={(event) => setInputFood(event.target.value)} value={inputFood} />
+          <div>
+            <button onClick={addFood}>  Submit </button>
+          </div>
+        </div>
+        <div>
+          {isSingleView && (
+            <div>
+              <Food
+                name={foodList[currIndex]}
+                index={currIndex}
+                foodList={foodList}
+                setFoodList={setFoodList}
+              >
+              </Food> /
+              <button onClick={nextFood}> Pass Me </button>
+            </div>
+          ) // why don't we pass in a props for Pass Me that makes Food buttons different when it isn't null?
+            // this will help us customize that the Food component looks different in single view!
+          }
+          {!isSingleView && // this is essentially the same thing, but when the return value of a function is 
+                            // the only line, we can remove the brackets and the return!
+            foodList.map((value, index) => <Food
+                name={value}
+                index={index}
+                foodList={foodList}
+                setFoodList={setFoodList}
+                likes={likes}
+                setLikes={setLikes}
+              >
+              </Food>
+            )
+          }
+        </div>
       </header>
     </div>
   );
 }
-
 export default App;
