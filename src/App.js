7fb49d66b08bcbccc8f888b64a9c59633876a267
Luke <77371718+ldalton02@users.noreply.github.com>
2021-05-09 22:06:02 -0700

    pushing new branch here

--- src/App.js
+++ src/App.js
@@ -7,12 +7,9 @@ import Food from './Food';
 
 
 
-
-
-
-
 function App() {
   const [foodList, setFoodList] = useState([]);
+  const [likeCount, setLikeCount] = useState([]);
   const [inputFood, setInputFood] = useState("");
 
   const [isSingleView, setIsSingleView] = useState(false);
@@ -20,8 +17,8 @@ function App() {
 
   const addFood = () => {
     setFoodList([...foodList, inputFood]);
-    setLikes([...likes, 0]);
     setInputFood("");
+    setLikeCount([...likeCount, 0]);
   }
 
   const switchInput = () => {
@@ -36,51 +33,72 @@ function App() {
     }
   }
 
+  const removeFood = () => {
+    if (currIndex == foodList.length - 1) {
+      setCurrIndex(0);
+    }
+    const foodCopy = [...foodList.splice(0, currIndex), ...foodList.splice(currIndex + 1)];
+    setFoodList(foodCopy);
+    const likeCopy = [...likeCount.splice(0, currIndex), ...likeCount.splice(currIndex + 1)];
+    setLikeCount(likeCopy);
+  } 
 
-  const [likes, setLikes] = useState([]);
   // true = single view?
   return (
     <div className="App">
       <header className="App-header">
         <div>
           {
-            <button onClick={switchInput}> {
+            <button class="float-switch-button my-button" onClick={switchInput}> {
               isSingleView ? "View List" : "View Single"
             } </button>
           }
-          <h2> Add a New Food</h2>
-          <input type="text" onChange={(event) => { setInputFood(event.target.value) }} value={inputFood} />
-          <div>
-            <button onClick={addFood}>  Submit </button>
-          </div>
-        </div>
-        <div>
-          {isSingleView && (
+          <div class="grid-container">
             <div>
-              <Food
-                name={foodList[currIndex]}
-                index={currIndex}
-                foodList={foodList}
-                setFoodList={setFoodList}
-              >
-              </Food>
-              <button onClick={nextFood}> Pass Me </button>
+              <h2 id="main-title"> Add a New Food</h2>
+              <input class="input-field" type="text" onChange={(event) => { setInputFood(event.target.value) }} value={inputFood} />
+              <div>
+                <button class="my-button" onClick={addFood}>  Submit </button>
+              </div>
             </div>
-          )
-          }
-          {!isSingleView &&
-            foodList.map((value, index) => {
-              return <Food
-                name={value}
-                index={index}
-                foodList={foodList}
-                setFoodList={setFoodList}
-                likes={likes}
-                setLikes={setLikes}
-              >
-              </Food>
-            })
-          }
+
+            <div>
+              { foodList.length == 0 &&
+                <h4 class="text-colorset"> Please enter a food! </h4>
+              }
+              {isSingleView && foodList.length > 0 &&  (
+                <div class="food-item">
+                  <Food
+                    name={foodList[currIndex]}
+                    index={currIndex}
+                    foodList={foodList}
+                    setFoodList={setFoodList}
+                    likeCount={likeCount}
+                    setLikeCount={setLikeCount}
+                  >
+                  </Food>
+                  <button class="my-button food-button" onClick={removeFood}>Remove Me</button>
+                  <button class="my-button food-button" onClick={nextFood}> Pass Me </button>
+                </div>
+              )
+              }
+              {!isSingleView && foodList.length > 0 && 
+                foodList.map((value, index) => {
+                  return  <div class="food-item"> <Food
+                    name={value}
+                    index={index}
+                    foodList={foodList}
+                    setFoodList={setFoodList}
+                    likeCount={likeCount}
+                    setLikeCount={setLikeCount}
+                  >
+                  </Food>
+                  <button class="my-button food-button" onClick={removeFood}>Remove Me</button>
+                  </div>
+                })
+              }
+            </div>
+          </div>
         </div>
       </header>
     </div>
--- src/Food.js
+++ src/Food.js
@@ -1,24 +1,17 @@
 import { useState } from "react";
 
 function Food(props) {
-    const removeFood = () => {
-        const foodCopy = [...props.foodList.splice(0, props.index), ...props.foodList.splice(props.index + 1)];
-        const likeCopy = [...props.likes.splice(0, props.index), ...props.likes.splice(props.index + 1)];
-        props.setFoodList(foodCopy);
-        props.setLikes(likeCopy);
+
+    const newAddLikes = () => {
+        var likeCopy = [...props.likeCount];
+        likeCopy[props.index] = likeCopy[props.index] + 1;
+        props.setLikeCount(likeCopy);
     }
 
-    const addLikes = () => {
-        var obj = {...props.likes};
-        obj[props.index] = 'a';
-        props.setLikes({obj});
-    }
-
-    return <div>
-        <h3>{props.name}</h3>
-        <h4 id="likeCount"> Likes: {props.likes[props.index]} </h4>
-        <button onClick={addLikes}>Like Me</button>
-        <button onClick={removeFood}>Remove Me</button>
+    return <div class="food-container">
+        <h3 class="food-item">{props.name}</h3>
+        <h4 id="likeCount" class="food-item"> Likes: {props.likeCount[props.index]} </h4>
+        <button class="my-button food-button" onClick={newAddLikes}>Like Me</button>
     </div>
 }
 export default Food;
