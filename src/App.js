12433df3b3d3ea0b13d154e55853763009ecd028
Luke <77371718+ldalton02@users.noreply.github.com>
2021-05-10 17:08:46 -0700

    resolves warnings/moves components around

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { useState } from 'react';
 import Food from './Food';
-
-
 
 
 function App() {
@@ -25,49 +22,31 @@ function App() {
     setIsSingleView(!isSingleView);
   }
 
-  const nextFood = () => {
-    if (currIndex < foodList.length - 1) {
-      setCurrIndex(currIndex + 1);
-    } else {
-      setCurrIndex(0);
-    }
-  }
-
-  const removeFood = () => {
-    if (currIndex == foodList.length - 1) {
-      setCurrIndex(0);
-    }
-    const foodCopy = [...foodList.splice(0, currIndex), ...foodList.splice(currIndex + 1)];
-    setFoodList(foodCopy);
-    const likeCopy = [...likeCount.splice(0, currIndex), ...likeCount.splice(currIndex + 1)];
-    setLikeCount(likeCopy);
-  } 
-
   // true = single view?
   return (
     <div className="App">
       <header className="App-header">
         <div>
           {
-            <button class="float-switch-button my-button" onClick={switchInput}> {
+            <button className="float-switch-button my-button" onClick={switchInput}> {
               isSingleView ? "View List" : "View Single"
             } </button>
           }
-          <div class="grid-container">
+          <div className="grid-container">
             <div>
               <h2 id="main-title"> Add a New Food</h2>
-              <input class="input-field" type="text" onChange={(event) => { setInputFood(event.target.value) }} value={inputFood} />
+              <input className="input-field" type="text" onChange={(event) => { setInputFood(event.target.value) }} value={inputFood} />
               <div>
-                <button class="my-button" onClick={addFood}>  Submit </button>
+                <button className="my-button" onClick={addFood}>  Submit </button>
               </div>
             </div>
 
             <div>
-              { foodList.length == 0 &&
-                <h4 class="text-colorset"> Please enter a food! </h4>
+              { foodList.length < 1 &&
+                <h4 className="text-colorset"> Please enter a food! </h4>
               }
               {isSingleView && foodList.length > 0 &&  (
-                <div class="food-item">
+                <div className="food-item">
                   <Food
                     name={foodList[currIndex]}
                     index={currIndex}
@@ -75,25 +54,26 @@ function App() {
                     setFoodList={setFoodList}
                     likeCount={likeCount}
                     setLikeCount={setLikeCount}
+                    setCurrIndex={setCurrIndex}
+                    isSingleView={isSingleView}
                   >
                   </Food>
-                  <button class="my-button food-button" onClick={removeFood}>Remove Me</button>
-                  <button class="my-button food-button" onClick={nextFood}> Pass Me </button>
                 </div>
               )
               }
               {!isSingleView && foodList.length > 0 && 
                 foodList.map((value, index) => {
-                  return  <div class="food-item"> <Food
+                  return  <div className="food-item"> <Food
                     name={value}
                     index={index}
                     foodList={foodList}
                     setFoodList={setFoodList}
                     likeCount={likeCount}
                     setLikeCount={setLikeCount}
+                    setCurrIndex={setCurrIndex}
+                    isSingleView={isSingleView}
                   >
                   </Food>
-                  <button class="my-button food-button" onClick={removeFood}>Remove Me</button>
                   </div>
                 })
               }
--- src/Food.js
+++ src/Food.js
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 function Food(props) {
 
     const newAddLikes = () => {
@@ -8,10 +6,43 @@ function Food(props) {
         props.setLikeCount(likeCopy);
     }
 
-    return <div class="food-container">
-        <h3 class="food-item">{props.name}</h3>
-        <h4 id="likeCount" class="food-item"> Likes: {props.likeCount[props.index]} </h4>
-        <button class="my-button food-button" onClick={newAddLikes}>Like Me</button>
+    const removeFood = () => {    
+        const foodCopy = [...props.foodList.splice(0, props.index), ...props.foodList.splice(props.index + 1)];
+        if (props.index === foodCopy.length) {
+            props.setCurrIndex(0);
+        }
+        props.setFoodList(foodCopy);
+
+        const likeCopy = [...props.likeCount.splice(0, props.index), ...props.likeCount.splice(props.index + 1)];
+        props.setLikeCount(likeCopy);
+    }
+
+    const nextFood = () => {
+        if (props.index < props.foodList.length - 1) {
+            props.setCurrIndex(props.index + 1);
+        } else {
+            props.setCurrIndex(0);
+        }
+    }
+
+    return <div className="food-container">
+        <h3 className="food-item">{props.name}</h3>
+        <h4 id="likeCount" className="food-item"> Likes: {props.likeCount[props.index]} </h4>
+
+        {props.isSingleView &&
+            <div>
+                <button className="my-button food-button" onClick={newAddLikes}>Like Me</button>
+                <button className="my-button food-button" onClick={nextFood}> Pass Me </button>
+            </div>
+        }
+
+        {!props.isSingleView &&
+            <div>
+                <button className="my-button food-button" onClick={newAddLikes}>Like Me</button>
+            </div>
+        }
+
+        <button className="my-button food-button" onClick={removeFood}>Remove Me</button>
     </div>
 }
 export default Food;
